3e720ebed45965dcfb47b06fa272bdf2dc39147b
nppe6 <xiao2313667401@foxmail.com>
2024-12-13 23:58:50 +0800

    全局组件注册封装

---
+++ src/components/form/xy-button.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <div>xy.button</div>
+</template>
+
+<style scoped></style>
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,6 +1,21 @@
 import { App } from 'vue'
 import { setupTailWindCss } from './tailwindcss'
+import _ from 'lodash'
 
 export function setupPlugins(app: App) {
+  autoRegisterComponent(app)
   setupTailWindCss()
+}
+
+export function autoRegisterComponent(app: App) {
+  const components = import.meta.glob('@/components/form/*.vue', {
+    eager: true,
+    import: 'default'
+  })
+
+  Object.keys(components).forEach((key) => {
+    const name = key.split('/').pop()?.split('.').shift()
+
+    app.component(_.camelCase(name), components[key] || {})
+  })
 }
